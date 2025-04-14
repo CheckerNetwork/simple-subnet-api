11eee5ad34d07d2c7d08149657a3b05f37a33a2a
Srdjan S <stankovic.srdjo@gmail.com>
2025-04-14 13:37:40 +0200

    Separate round and tasking service

--- test/round-service.test.js
+++ test/round-service.test.js
@@ -4,6 +4,7 @@ import { createPgPool } from '../lib/pool.js'
 import { migrateWithPgClient } from '../lib/migrate.js'
 import { DATABASE_URL } from '../lib/config.js'
 import { RoundService } from '../lib/round-service.js'
+import { TaskingService } from '../lib/tasking-service.js'
 
 const DEFAULT_CONFIG = {
   roundDurationMs: 1000,
@@ -15,10 +16,15 @@ const DEFAULT_CONFIG = {
 describe('RoundService', () => {
   /** @type {import('pg').Pool} */
   let pgPool
+  /** @type {TaskingService} */
+  let taskingService
 
   before(async () => {
     pgPool = await createPgPool(DATABASE_URL)
     await migrateWithPgClient(pgPool)
+    taskingService = new TaskingService(pgPool, {
+      maxTasks: DEFAULT_CONFIG.maxTasks
+    })
   })
 
   after(async () => {
@@ -28,31 +34,11 @@ describe('RoundService', () => {
   beforeEach(async () => {
     // Reset the database state before each test
     await pgPool.query('DELETE FROM checker_rounds')
-    await pgPool.query('DELETE FROM checker_subnet_tasks')
-  })
-
-  describe('registerTaskSampler', () => {
-    it('should register a task sampler for a subnet', () => {
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
-      const samplerFn = () => { }
-      roundService.registerTaskSampler('subnet1', samplerFn)
-
-      assert.doesNotThrow(() => roundService.registerTaskSampler('subnet1', samplerFn))
-    })
-
-    it('should throw an error if samplerFn is not a function', () => {
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
-      assert.throws(
-        // @ts-ignore
-        () => roundService.registerTaskSampler('subnet1', null),
-        /Task sampler for subnet subnet1 must be a function/
-      )
-    })
   })
 
   describe('rounds', () => {
     it('should create a new round if no active round exists', async () => {
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
+      const roundService = new RoundService(pgPool, taskingService, DEFAULT_CONFIG)
 
       await roundService.start()
       roundService.stop()
@@ -70,7 +56,7 @@ describe('RoundService', () => {
         VALUES ($1, $2, $3, $4)
       `, [now, endTime, DEFAULT_CONFIG.maxTasksPerNode, true])
 
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
+      const roundService = new RoundService(pgPool, taskingService, DEFAULT_CONFIG)
 
       await roundService.start()
       roundService.stop()
@@ -81,7 +67,7 @@ describe('RoundService', () => {
     })
 
     it('should stop the round service and prevent further round checks', async () => {
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
+      const roundService = new RoundService(pgPool, taskingService, DEFAULT_CONFIG)
 
       await roundService.start()
       roundService.stop()
@@ -97,54 +83,6 @@ describe('RoundService', () => {
     })
   })
 
-  describe('task generation', () => {
-    it('should generate tasks for all registered subnets during a round', async () => {
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
-
-      roundService.registerTaskSampler('subnet1', async () => [
-        { payloadId: 'task1', nodeId: 'node1' },
-        { payloadId: 'task2', nodeId: 'node2' }
-      ])
-
-      roundService.registerTaskSampler('subnet2', async () => [
-        { payloadId: 'task3', nodeId: 'node3' }
-      ])
-
-      // roundService.registerTaskSampler('subnet2', async () => {
-      //   throw new Error('Error generating tasks')
-      // })
-
-      await roundService.start()
-      roundService.stop()
-
-      const { rows: rounds } = await pgPool.query('SELECT * FROM checker_rounds WHERE active = true')
-      const activeRoundId = rounds[0].id
-
-      const { rows: tasks } = await pgPool.query('SELECT * FROM checker_subnet_tasks WHERE round_id = $1', [activeRoundId])
-      assert.strictEqual(tasks.length, 3)
-
-      const taskPayloads = tasks.map(task => task.task_definition)
-      assert.deepStrictEqual(taskPayloads.sort(), [
-        { payloadId: 'task1', nodeId: 'node1' },
-        { payloadId: 'task2', nodeId: 'node2' },
-        { payloadId: 'task3', nodeId: 'node3' }
-      ])
-    })
-
-    it('should not generate tasks if no samplers are registered', async () => {
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
-
-      await roundService.start()
-      roundService.stop()
-
-      const { rows: rounds } = await pgPool.query('SELECT * FROM checker_rounds WHERE active = true')
-      const activeRoundId = rounds[0].id
-
-      const { rows: tasks } = await pgPool.query('SELECT * FROM checker_subnet_tasks WHERE round_id = $1', [activeRoundId])
-      assert.strictEqual(tasks.length, 0)
-    })
-  })
-
   describe('round transitions', () => {
     it('should deactivate the old round and create a new one when the current round ends', async () => {
       const now = new Date()
@@ -154,7 +92,7 @@ describe('RoundService', () => {
         VALUES ($1, $2, $3, $4)
       `, [now, endTime, DEFAULT_CONFIG.maxTasksPerNode, true])
 
-      const roundService = new RoundService(pgPool, DEFAULT_CONFIG)
+      const roundService = new RoundService(pgPool, taskingService, DEFAULT_CONFIG)
 
       await roundService.start()
 
