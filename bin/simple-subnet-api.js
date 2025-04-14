7e0a4e2f2529a929f964a969da7aa2a1d99e4b93
Srdjan S <stankovic.srdjo@gmail.com>
2025-04-14 13:54:32 +0200

    Remove tasking implementation

--- bin/simple-subnet-api.js
+++ bin/simple-subnet-api.js
@@ -1,43 +1,6 @@
 import '../lib/instrument.js'
 import { createApp } from '../lib/app.js'
 import { DATABASE_URL, HOST, PORT, REQUEST_LOGGING, poolConfig } from '../lib/config.js'
-import { RoundService } from '../lib/round-service.js'
-import { createPgPool } from '../lib/pool.js'
-import { TaskingService } from '../lib/tasking-service.js'
-
-const pool = await createPgPool(DATABASE_URL)
-const taskingService = new TaskingService(
-  pool,
-  {
-    maxTasks: 100
-  }
-)
-const roundService = new RoundService(
-  pool,
-  taskingService,
-  {
-    roundDurationMs: 1 * 60 * 1000, // 20 minutes
-    maxTasksPerNode: 10,
-    checkRoundIntervalMs: 1000 // 1 minute
-  }
-)
-
-roundService.start().catch((error) => {
-  console.error('Failed to start round service:', error)
-  process.exit(1)
-})
-
-process.on('SIGINT', async () => {
-  console.log('Stopping round service...')
-  roundService.stop()
-  process.exit(0)
-})
-
-process.on('SIGTERM', async () => {
-  console.log('Stopping round service...')
-  roundService.stop()
-  process.exit(0)
-})
 
 const app = createApp({
   databaseUrl: DATABASE_URL,
