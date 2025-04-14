91fa889b416715d7379c476655e53ba966f031e8
Srdjan S <stankovic.srdjo@gmail.com>
2025-04-14 13:55:47 +0200

    Remove checker tasks table

--- migrations/004.do.checker-subnet-tasks.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS checker_subnet_tasks (
-    id BIGSERIAL PRIMARY KEY,
-    subnet TEXT NOT NULL,
-    round_id BIGINT NOT NULL,
-    task_definition JSONB NOT NULL,
-    FOREIGN KEY (round_id) REFERENCES checker_rounds (id) ON DELETE CASCADE
-);
